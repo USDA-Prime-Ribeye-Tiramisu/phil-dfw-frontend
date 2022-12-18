08553cede121e52b91ef9bdb7a4d683b251b7cbf
Phil <plrtn@protonmail.com>
2022-12-18 07:53:24 -0500

    initial commit for types

---
+++ src/data/types.ts
@@ -0,0 +1,28 @@
+export interface Point {
+  latitude: number
+  longitude: number
+}
+
+export interface Zone {
+  center: Point
+  radius : number
+  totalPopulation: number
+  medianIncome: number
+  composition?: Tract[]
+}
+
+export interface Tract {
+  id : string
+  geojson? : object
+}
+
+const _interpolationStrategy = {
+  "CENTROID" : 1,
+  "AREAL_PROPORTION" : 2
+} as const 
+
+export type InterpolationStrategy = keyof typeof _interpolationStrategy
+
+export function getIdFromInterpolationStrategy(is : InterpolationStrategy) : number {
+  return _interpolationStrategy[is]
+}
