0551339ec02040660bf9f2b1237bc6ce5159021c
Phil <plrtn@protonmail.com>
2022-12-20 11:35:20 -0500

    fix broken tests

--- test/dfwDataService.test.ts
+++ test/dfwDataService.test.ts
@@ -1,9 +1,9 @@
 import {assert, expect, test } from 'vitest'
-import { getShapes, getZoneData } from '../src/services/dfwDataService'
+import { tractsUrl, zoneUrl, tractsFetcher, zoneDataFetcher  } from '../src/services/dfwDataService'
 import { Zone, Tract } from '../src/data/types'
 
 test('fetch geojson from backend', async () => {
-  const result : Tract[] = await getShapes() 
+  const result : Tract[] = await tractsFetcher()(tractsUrl)
   assert(result.length > 0)
   assert(
     result.reduce((acc, cur) => acc && !!cur.geojson, true)
@@ -11,6 +11,6 @@ test('fetch geojson from backend', async () => {
 })
 
 test('fetch computed value from backend', async () => {
-  const result : Zone = await getZoneData({ latitude: 33.045352, longitude: -96.781508 }, 2000, "CENTROID")
+  const result : Zone = await zoneDataFetcher()(zoneUrl({ latitude: 33.045352, longitude: -96.781508 }, 2000, "CENTROID"))
   assert(result.totalPopulation && result.medianIncome)
 })
